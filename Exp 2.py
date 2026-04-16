b927de2fb0ae3d148854232f28e9aeec753c82bf
Sangeeta0823 <sangeeta@eng.rizvi.edu.in>
2026-04-16 18:25:07 +0530

    Exp 2.py
    Add area calculation for circle, rectangle, and triangle

---
+++ Exp 2.py
@@ -0,0 +1,19 @@
+print("1 circle")
+print("2 rectangle")
+print("3 triangle")
+
+c= int(input("choose"))
+
+if c == 1:
+    r=float(input("radius:"))
+    print("area=",3.14*r*r)
+
+elif c==2:
+    l=float(input("length"))
+    b-float(input("bradth"))
+    print("area=",l*b)
+
+elif c==3:
+    l=float(input("length"))
+    b=float(input("base"))
+    print("area",0.5*l*b)
