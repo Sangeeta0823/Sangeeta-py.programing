47394e53c872226e88fe83bb690ae22898ca7646
Sangeeta0823 <sangeeta@eng.rizvi.edu.in>
2026-04-16 18:29:23 +0530

    Exp 6.py

---
+++ Exp 6.py
@@ -0,0 +1,6 @@
+cet = {"sangeeta","vidya"}
+jee = {"mudra","sangeeta"}
+neet={"sana","vidya"}
+print("all division:",cet|jee|neet)
+print("students in all exam:",cet & jee & neet)
+print("cet but not in jee :",cet-jee)
